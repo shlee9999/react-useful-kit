a179a75e0f04685124e2caa09363f8db8c75b877
shlee9999 <oklshop555@naver.com>
2025-07-29 22:08:04 +0900

    feat: useLockBodyScroll 훅 및 객체 유틸리티 함수 내보내기 추가
    - index.ts에 useLockBodyScroll 훅 추가
    - global에서 modalAlert 내보내기 추가
    - 객체 유틸리티(pick, omit) 함수 내보내기 추가

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export { default as useDeepEffect } from './hooks/useDeepEffect'
 export { default as useDeepMemo } from './hooks/useDeepMemo'
 export { default as useDeepCallback } from './hooks/useDeepCallback'
+export { default as useLockBodyScroll } from './hooks/useLockBodyScroll'
 
 // Context
 export { AlertProvider } from './context/AlertProvider'
@@ -12,3 +13,5 @@ export type { AlertOptions } from './types/alert-options'
 
 // Utils
 export { deepEqual } from './utils/deepEqual'
+export { modalAlert } from './global'
+export { pick, omit } from './utils/objectUtils'
