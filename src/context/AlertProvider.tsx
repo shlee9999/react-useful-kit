5edb7288bb6bbaa677b18551c470aa7060dd8431
shlee9999 <oklshop555@naver.com>
2025-07-30 21:06:11 +0900

    feat: AlertProvider에 다중 사용 및 기본값 설정에 대한 주석 추가
    - AlertProvider 사용 시 다중 인스턴스 지원을 위한 주석 추가
    - 기본값으로 'react-useful-kit-modal-container' 사용에 대한 설명 포함

--- src/context/AlertProvider.tsx
+++ src/context/AlertProvider.tsx
@@ -4,6 +4,28 @@ import type { AlertOptions } from '@/types/alert-options'
 import { useCallback, useMemo, useState, useRef, type ReactNode, Fragment } from 'react'
 import { AlertContext } from './AlertContext'
 
+/**
+ * 기본적으로 main.tsx에서 App을 감싸주세요.
+ * 특정 Context를 공유하고 싶다면, 해당 ContextProvider 내부에 AlertProvider를 사용하세요.
+ * 즉, 다중 AlertProvider를 지원합니다. id를 지정하지 않으면 기본값으로 'react-useful-kit-modal-container'를 사용합니다.
+ *
+ * @example
+ * ```tsx
+ * <AlertProvider>
+ *   <App /> //* 기본값으로 'react-useful-kit-modal-container'를 사용합니다.
+ * </AlertProvider>
+ * ```
+ *
+ * 다중 AlertProvider를 사용하고 싶다면, id를 지정하는 게 유지보수에 좋습니다.
+ * @example
+ * ```tsx
+ * <SomeContext>
+ *   <AlertProvider id='custom-modal-container'>
+ *     <Child />
+ *   </AlertProvider>
+ * </SomeContext>
+ * ```
+ */
 export const AlertProvider = ({ children, id }: { children: ReactNode; id?: string }) => {
   const [modals, setModals] = useState<ReactNode[]>([])
   const containerRef = useRef<HTMLDivElement>(null)
